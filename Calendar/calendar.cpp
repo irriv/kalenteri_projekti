f162186f1cd1a5a2df479e64d4b834bd78ba2e5a
viljami_irri <viljami.irri@tuni.fi>
2022-11-19 22:26:21 +0200

    ui and text browser test

--- Calendar/calendar.cpp
+++ Calendar/calendar.cpp
@@ -6,10 +6,17 @@ Calendar::Calendar(QWidget *parent)
     , ui(new Ui::Calendar)
 {
     ui->setupUi(this);
+    data.insert(std::pair<QDate,QString*>(*new QDate(2022, 11, 19), new QString("test")));
 }
 
 Calendar::~Calendar()
 {
     delete ui;
+}
+
+
+void Calendar::on_calendarWidget_clicked(const QDate &date)
+{
+    ui->textBrowser->setText(*data.at(date));
 }
 
--- Calendar/calendar.hh
+++ Calendar/calendar.hh
@@ -2,6 +2,7 @@
 #define CALENDAR_HH
 
 #include <QMainWindow>
+#include <map>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Calendar; }
@@ -15,7 +16,11 @@ public:
     Calendar(QWidget *parent = nullptr);
     ~Calendar();
 
+private slots:
+    void on_calendarWidget_clicked(const QDate &date);
+
 private:
     Ui::Calendar *ui;
+    std::map<QDate,QString*> data;
 };
 #endif // CALENDAR_HH
