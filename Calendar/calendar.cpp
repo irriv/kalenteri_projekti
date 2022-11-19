5e4829df8962ff21202a25140557f6d9c7c05503
viljami_irri <viljami.irri@tuni.fi>
2022-11-19 23:45:42 +0200

    ui changes, implementation ready except file handling

--- Calendar/calendar.cpp
+++ Calendar/calendar.cpp
@@ -6,7 +6,8 @@ Calendar::Calendar(QWidget *parent)
     , ui(new Ui::Calendar)
 {
     ui->setupUi(this);
-    data.insert(std::pair<QDate,QString*>(*new QDate(2022, 11, 19), new QString("test")));
+    //read data from file
+    emit ui->calendarWidget->clicked(QDate::currentDate());
 }
 
 Calendar::~Calendar()
@@ -17,6 +18,84 @@ Calendar::~Calendar()
 
 void Calendar::on_calendarWidget_clicked(const QDate &date)
 {
-    ui->textBrowser->setText(*data.at(date));
+    auto it = data.find(date);
+    if(it != data.end()){
+        ui->textBrowser->setText(QString::fromStdString(it->second));
+    }
+    else{
+        clear_text_browser();
+    }
+}
+
+
+void Calendar::on_editButton_clicked()
+{
+    ui->dateLabel->setText("Now editing: " + ui->calendarWidget->selectedDate().toString("dd.MM.yyyy"));
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void Calendar::on_deleteButton_clicked()
+{
+    if(!create_confirmation_message("Are you sure you want to delete the contents?")){
+        clear_text_browser();
+        data.erase(data.find(ui->calendarWidget->selectedDate()));
+        //add write to file
+    }
+}
+
+
+void Calendar::on_quitButton_clicked()
+{
+    if(create_confirmation_message("Are you sure you want to quit?")){
+        close();
+    }
+}
+
+
+void Calendar::on_saveButton_clicked()
+{
+    QDate date = ui->calendarWidget->selectedDate();
+    QString qstring = ui->plainTextEdit->toPlainText();
+    add_entry(date, qstring);
+    emit ui->calendarWidget->clicked(date);
+    navigate_to_calendar();
+    //add write to file
+}
+
+
+void Calendar::on_cancelButton_clicked()
+{
+    if(create_confirmation_message("Are you sure you want to cancel?")){
+        navigate_to_calendar();
+    }
+}
+
+
+void Calendar::clear_text_browser(){
+    ui->textBrowser->clear();
+}
+
+
+void Calendar::add_entry(QDate date, QString qstring)
+{
+    data.insert(std::pair<QDate,std::string>(date,qstring.toStdString()));
+}
+
+
+void Calendar::navigate_to_calendar()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+    ui->plainTextEdit->clear();
+}
+
+
+bool Calendar::create_confirmation_message(std::string text)
+{
+    auto reply = QMessageBox::question(ui->stackedWidget->currentWidget(),"",QString::fromStdString(text));
+    if(reply == QMessageBox::Yes){
+        return true;
+    }
+    return false;
 }
 
--- Calendar/calendar.hh
+++ Calendar/calendar.hh
@@ -2,6 +2,7 @@
 #define CALENDAR_HH
 
 #include <QMainWindow>
+#include <QMessageBox>
 #include <map>
 
 QT_BEGIN_NAMESPACE
@@ -19,8 +20,23 @@ public:
 private slots:
     void on_calendarWidget_clicked(const QDate &date);
 
+    void on_editButton_clicked();
+
+    void on_deleteButton_clicked();
+
+    void on_quitButton_clicked();
+
+    void on_saveButton_clicked();
+
+    void on_cancelButton_clicked();
+
 private:
+    void clear_text_browser();
+    void add_entry(QDate date, QString qstring);
+    void navigate_to_calendar();
+    bool create_confirmation_message(std::string text);
+
     Ui::Calendar *ui;
-    std::map<QDate,QString*> data;
+    std::map<QDate,std::string> data;
 };
 #endif // CALENDAR_HH
